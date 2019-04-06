b077a5acc93ad5a3a6b2c245a6563ed9b1c9c036
im-light <47765034+im-light@users.noreply.github.com>
2019-04-05 17:21:59 -0700

    programaMD

--- tarea4/Program.cs
+++ tarea4/Program.cs
@@ -1,8 +1,4 @@
-# Tarea4
-
-1. Considera el siguiente fragmento de programa:
-```c sharp
-using System;
+﻿using System;
 
 namespace tarea4
 {
@@ -44,12 +40,3 @@ namespace tarea4
         }
     }
 }
-```
-1.1. ¿Qué valores imprimen las lineas (1) y (2) ?
-
-1.2.  Redefine el método Display en el espacio indicado,
-
-     una vez redefinido el método, ¿qué valores imprimen las lineas (1) y (2) ?.
-
-1.3. ¿Que palabra debes agregar en la linea (public _______ string Display()) al definir A.Display()?
-
